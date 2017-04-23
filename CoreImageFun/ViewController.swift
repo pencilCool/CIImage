ff3feb5d8c578b22742ecf4cd5b3ffae8f5747a7
pencilCool <yhtang_xt@163.com>
2017-04-23 10:36:33 +0800

    add old filter

--- CoreImageFun/ViewController.swift
+++ CoreImageFun/ViewController.swift
@@ -58,11 +58,11 @@ class ViewController: UIViewController , UINavigationControllerDelegate, UIImage
     {
         let sliderValue = sender.value
         
-        filter.setValue(sliderValue, forKey: kCIInputIntensityKey)
-        let outputImage = filter.outputImage
+//        filter.setValue(sliderValue, forKey: kCIInputIntensityKey)
+//        let outputImage = filter.outputImage!
+        let outputImage = self.oldPhoto(img: beginImage, withAmount: sliderValue)
         
-        let cgimg = context.createCGImage(outputImage!, from: outputImage!.extent)
-        
+        let cgimg = context.createCGImage(outputImage, from: outputImage.extent)
         let newImage = UIImage(cgImage: cgimg!, scale: 1, orientation: orientation);
         self.imageView.image = newImage
         
@@ -107,7 +107,38 @@ class ViewController: UIViewController , UINavigationControllerDelegate, UIImage
         }
     }
     
-    
+    func oldPhoto(img: CIImage, withAmount intensity: Float) -> CIImage {
+        // 1
+        let sepia = CIFilter(name:"CISepiaTone")!
+        sepia.setValue(img, forKey:kCIInputImageKey)
+        sepia.setValue(intensity, forKey:"inputIntensity")
+        
+        // 2
+        let random = CIFilter(name:"CIRandomGenerator")!
+        
+        // 3
+        let lighten = CIFilter(name:"CIColorControls")!
+        lighten.setValue(random.outputImage, forKey:kCIInputImageKey)
+        lighten.setValue(1 - intensity, forKey:"inputBrightness")
+        lighten.setValue(0, forKey:"inputSaturation")
+        
+        // 4
+        let croppedImage = lighten.outputImage?.cropping(to: beginImage.extent)
+        
+        // 5
+        let composite = CIFilter(name:"CIHardLightBlendMode")!
+        composite.setValue(sepia.outputImage, forKey:kCIInputImageKey)
+        composite.setValue(croppedImage, forKey:kCIInputBackgroundImageKey)
+        
+        // 6
+        let vignette = CIFilter(name:"CIVignette")!
+        vignette.setValue(composite.outputImage, forKey:kCIInputImageKey)
+        vignette.setValue(intensity * 2, forKey:"inputIntensity")
+        vignette.setValue(intensity * 30, forKey:"inputRadius")
+        
+        // 7
+        return vignette.outputImage!
+    }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         self.dismiss(animated: true, completion: nil);
